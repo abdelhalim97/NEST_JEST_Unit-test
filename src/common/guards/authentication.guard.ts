cad929fb771a5bf285ad12b84222e63cd9d1515f
Abdelhalim Ben Oun <abdelhalimbenoun@gmail.com>
2024-03-17 21:37:36 +0100

    update auth guard

--- src/common/guards/authentication.guard.ts
+++ src/common/guards/authentication.guard.ts
@@ -1,7 +1,8 @@
 import { EnvironmentService } from 'src/common/services/environment.service';
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { CanActivate, ExecutionContext, HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { TokenPayloadResponse } from 'src/common/responses/token-payload.response';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -13,17 +14,17 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if (!token) {
-      throw new UnauthorizedException();
-    }
+
+    if (!token) throw new HttpException('Unauthorized!', HttpStatus.UNAUTHORIZED);
+
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
+      const payload: TokenPayloadResponse = await this.jwtService.verifyAsync(token, {
         secret: this.environmentService.jasonWebTokenConfig.JWTSecretKey,
       });
 
       request['user'] = payload;
     } catch {
-      throw new UnauthorizedException();
+      throw new HttpException('Unauthorized!', HttpStatus.UNAUTHORIZED);
     }
     return true;
   }
