5c2acb21eb5b6c22cbe782236d427698afab85e0
Abdelhalim Ben Oun <abdelhalimbenoun@gmail.com>
2024-03-25 22:13:32 +0100

    auth unit test(ujncomplete)

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,43 @@
+import { MongoClient, Db } from 'mongodb';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { RegisterUserDto } from 'src/modules/auth/dto/register-user.dto';
+import { UsersService } from 'src/modules/users/users.service';
 
 describe('AuthService', () => {
+  let db: Db;
+  let connection: MongoClient;
   let service: AuthService;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(String(global.__MONGO_URI__));
+    db = connection.db();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, UsersService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  it('register user test', async () => {
+    const registerUserDto: RegisterUserDto = {
+      name: 'John',
+      email: 'John@email.com',
+      password: '123456',
+      confirmPassword: '123456',
+      phone: '235689',
+    };
+    const users = db.collection('users');
+
+    await users.insertOne(registerUserDto);
+
     expect(service).toBeDefined();
   });
 });
