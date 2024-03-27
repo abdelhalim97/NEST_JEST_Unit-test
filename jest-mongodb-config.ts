d1712c933e34e1b94f23a3c4a4d23c0658292ee6
Abdelhalim Ben Oun <abdelhalimbenoun@gmail.com>
2024-03-27 22:07:05 +0100

    fix jest config

--- jest-mongodb-config.ts
+++ jest-mongodb-config.ts
@@ -7,4 +7,5 @@ module.exports = {
     autoStart: false,
     instance: {},
   },
+  useSharedDBForAllJestWorkers: false,
 };
--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  modulePaths: ['<rootDir>'],
+  rootDir: './',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
+  watchPathIgnorePatterns: ['globalConfig'],
 };
