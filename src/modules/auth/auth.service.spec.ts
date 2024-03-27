291a31f4f7a02e906e31aa7f81b3d891e04cbb81
Abdelhalim Ben Oun <abdelhalimbenoun@gmail.com>
2024-03-28 00:37:49 +0100

    delete app service

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -3,6 +3,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from 'src/modules/auth/dto/register-user.dto';
 import { UsersService } from 'src/modules/users/users.service';
+import { ForgotPasswordsService } from 'src/modules/forgot-passwords/forgot-passwords.service';
+import { EnvironmentService } from 'src/common/services/environment.service';
+import { CommonService } from 'src/common/services/common.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/modules/users/user.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { ForgotPassword } from 'src/modules/forgot-passwords/forgot-password.schema';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AuthService', () => {
   let db: Db;
@@ -15,8 +23,21 @@ describe('AuthService', () => {
   });
 
   beforeEach(async () => {
+    const users = db.collection(User.name);
+    const forgotPasswords = db.collection(ForgotPassword.name);
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, UsersService],
+      providers: [
+        AuthService,
+        UsersService,
+        ForgotPasswordsService,
+        { provide: getModelToken(ForgotPassword.name), useValue: forgotPasswords },
+        EnvironmentService,
+        CommonService,
+        JwtService,
+        { provide: getModelToken(User.name), useValue: users },
+      ],
+      imports: [ConfigModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -34,7 +55,7 @@ describe('AuthService', () => {
       confirmPassword: '123456',
       phone: '235689',
     };
-    const users = db.collection('users');
+    const users = db.collection(User.name);
 
     await users.insertOne(registerUserDto);
 
