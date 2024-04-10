a69774651d0eb7be654e68e18d0b47e534b3098b
Abdelhalim Ben Oun <abdelhalimbenoun@gmail.com>
2024-04-10 15:51:28 +0100

    users unit test

---
+++ src/modules/auth/dto/stubs/update-password.stub.ts
@@ -0,0 +1,6 @@
+import { UpdatePasswordDto } from 'src/modules/auth/dto/update-password.dto';
+
+export const updatePasswordDtoStub: UpdatePasswordDto = {
+  password: '123456',
+  confirmPassword: '123456',
+};
